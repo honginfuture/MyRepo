0154e5d2ac181309b5a6a67414ed05ff71e778e0
rchchan <rchchan@01469-RCHCHAN.ogcio.hksarg>
2021-09-08 13:32:27 +0800

    upload TextReader

--- Hello/src/GUI.java
+++ Hello/src/GUI.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,22 +11,19 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /******************************************************************************
- *  Compilation:  javac GUI.java
- *  Execution:    java GUI
+ * Compilation: javac GUI.java Execution: java GUI
  *
- *  A minimal Java program with a graphical user interface. The
- *  GUI prints out the number of times the user clicks a button.
+ * A minimal Java program with a graphical user interface. The GUI prints out
+ * the number of times the user clicks a button.
  *
- *  % java GUI
- *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
+ * % java GUI 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
  ******************************************************************************/
 
-
-
 public class GUI {
-	private int clicks = 0;
+	private double clicks = 0;
 	private JLabel label = new JLabel("Number of clicks:  0     ");
 	private JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
 
 	public GUI() {
 
@@ -49,14 +47,35 @@ public class GUI {
 
 		// the clickable button3 to ShowText
 		JButton button3 = new JButton("ClickMe");
-		button2.addActionListener(new ActionListener() {
+		button3.addActionListener(new ActionListener() {
+			String s = "<html>";
+
 			public void actionPerformed(ActionEvent e) {
-				label.setText("Surprise!");
+				for (int i = 0; i <= 10; i++) {
+					s += (i + "\t" + i + "\t" + 2 * Math.PI * i / 2 + "<br>");
+				}
+				s += "</html>";
+
+				panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 30));
+				panel.setLayout(new GridLayout(0, 1));
+				panel.add(button1);
+				panel.add(button2);
+				panel.add(button3);
+				panel.add(label);
+
+				// set up the frame and display it
+				frame.add(panel, BorderLayout.CENTER);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setTitle("GUI");
+				frame.pack();
+				frame.setVisible(true);
+
+				label.setText(s);
 			}
 		});
 
 		// the panel with the button and text
-		JPanel panel = new JPanel();
+
 		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
 		panel.setLayout(new GridLayout(0, 1));
 		panel.add(button1);
