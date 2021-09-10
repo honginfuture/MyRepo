1266a8ea596be4fdffdb8f40dfd0f0fb37c87df4
rchchan <rchchan@01469-RCHCHAN.ogcio.hksarg>
2021-09-10 15:33:02 +0800

    Update

---
+++ Hello/src/PersonInterface.java
@@ -0,0 +1,12 @@
+
+public interface PersonInterface {
+	public void walk();
+
+	public void deposit();
+
+	public boolean getTiredness();
+
+	public void setTiredness();
+
+
+}
--- Hello/src/Waitress.java
+++ Hello/src/Waitress.java
@@ -1,8 +1,23 @@
 public class Waitress extends Person {
 
-    public String serveStarter(String starter) {
-        return "Serving a " + starter;
-    }
-    
-    // additional methods/constructors
+	public Waitress(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void serveStarter(String starter) {
+		System.out.println("Serving a " + starter);
+	}
+
+	@Override
+	public void drink() {
+		super.drink();
+		System.out.println("Waitress: I am drinking coffee!");
+	}
+
+	@Override
+	public void walk() {
+		System.out.println("Waitress: I dont wanna walk!");
+	}
+	// additional methods/constructors
 }
