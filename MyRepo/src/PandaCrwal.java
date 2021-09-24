d0bdd41e74ac5c640d6f431f381a58ba3a664bc9
rchchan <rchchan@01469-RCHCHAN.ogcio.hksarg>
2021-09-24 18:24:37 +0800

    Correct Promotion!

--- MyRepo/src/PandaCrwal.java
+++ MyRepo/src/PandaCrwal.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.v85.page.Page.GetAppManifestResponse;
@@ -18,26 +23,97 @@ public class PandaCrwal {
 		driver.navigate().to("\n"
 				+ "		https://www.foodpanda.hk/restaurants/new?lat=22.2742239&lng=114.1728051&vertical=shop&expedition=delivery/");
 		Document doc = Jsoup.parse(driver.getPageSource());
-		// https://www.twilio.com/blog/working-with-html-on-the-web-java-jsoup
-		// Get content by Class
+		Elements divElements = doc.select("span[class=\"box-flex fd-row ai-center\"],span[class=\"badge-info\"]");
+		Elements nameElements = doc.select("span[class=\"name fn\"]");
+		StringBuilder builder = new StringBuilder();
+		Iterator<Element> iterator = divElements.iterator();
+		Iterator<Element> nameIterator = nameElements.iterator();
+		System.out.println("number of Shop: " + nameElements.size());
+		System.out.println("number of RawPromotion: " + divElements.size());
+		int totalCount = 0;
+		int promoCount = 0;
+		int dryCount = 0;
 
-		// System.out.println(doc.select("div>class.info-badges-without-variant-layout>span>span").html());
+		ArrayList<Element> divElementsArr = new ArrayList<Element>();
+		int i = 0;
+		while (iterator.hasNext()) {
+			divElementsArr.add(iterator.next());
+			// System.out.println(divElementsArr.get(i).text());
+			i++;
+		}
 
-		String[] shopNames = new String[doc.getElementsByClass("name fn").size()];
-		String[] shopPromos = new String[shopNames.length];
-		System.out.println(shopNames.length + " " + shopPromos.length);
-		for (int i = 0; i < doc.getElementsByClass("name fn").size(); i++) {
-			shopNames[i] = doc.getElementsByClass("name fn").get(i).text();
-
-			if (i < (doc.getElementsByClass("box-flex fd-row ai-center").size())) {
-				shopPromos[i] = doc.getElementsByClass("box-flex fd-row ai-center").get(i).text();
-				System.out.println("Space\t:" + i + "/" + shopNames.length + ",  \t" + shopNames[i] + "\t\tPromo\t\t:"
-						+ i + "/" + shopPromos.length + "\t\tPromo\t\t:" + shopPromos[i]);
-			} else {
-				System.out.println("Space\t:" + i + "/" + shopNames.length + ",  \t" + shopNames[i]);
+		for (int t = 0; t < divElementsArr.size() - 1; t++) {
+			if (divElementsArr.get(t).attr("class").equals("box-flex fd-row ai-center")
+					&& divElementsArr.get(t + 1).attr("class").equals("badge-info")) {
+				promoCount++;
+				System.out.print(divElementsArr.get(t).attr("class").toString() + "\t");
+				System.out.println(promoCount + ":\t\tPromotion");
+			} else if 
+					(
+//							(divElementsArr.get(t).attr("class").equals("badge-info")
+//					&& divElementsArr.get(t + 1).attr("class").equals("box-flex fd-row ai-center")) 
+//					|| 
+					(divElementsArr.get(t).attr("class").equals("badge-info")
+					&& divElementsArr.get(t + 1).attr("class").equals("badge-info"))) {
+				dryCount++;
+				System.out.print(divElementsArr.get(t).attr("class").toString() + "\t");
+				System.out.println(dryCount + ":\t\t\t\tNo Promotion");
 			}
 		}
+
+//		while (iterator.hasNext()) {
+//			Element divElement = divElements.get(totalCount);
+//			builder.append(divElement.attr("class").toString() + "\r\n");
+//			divElement = iterator.next();
+//			totalCount++;
+//		}
+//		System.out.println(totalCount + "\r\n" + builder.toString());
+
+//		while (iterator.hasNext()) {
+//			Element nEl = iterator.next();
+//
+//			// Element nameElement = nameIterator.next();
+//			if (nEl.attr("class").equals("box-flex fd-row ai-center") && nnEl.attr("class").equals("badge-info")) {
+//				promoCount++;
+//				System.out.print(nEl.attr("class").toString() + "\t");
+//				System.out.println(promoCount + ":\t\tPromotion");
+//				totalCount++;
+//
+//			} else if (nnEl.attr("class").equals("box-flex fd-row ai-center")
+//					&& nEl.attr("class").equals("badge-info")) {
+//				dryCount++;
+//				System.out.print(nEl.attr("class").toString());
+//				System.out.println(dryCount + ":\t\t\t\tNo Promotion");
+//				totalCount++;
+//			}
+//		}
+//		System.out.print(promoCount + " promotion!");
+//		System.out.print(dryCount + "NO PROMOTION");
+
 	}
+}
+
+// System.out.println(count + builder.toString());
+
+// https://www.twilio.com/blog/working-with-html-on-the-web-java-jsoup
+// Get content by Class
+
+// System.out.println(doc.select("div>class.info-badges-without-variant-layout>span>span").html());
+
+//		String[] shopNames = new String[doc.getElementsByClass("name fn").size()];
+//		String[] shopPromos = new String[shopNames.length];
+//		System.out.println(shopNames.length + " " + shopPromos.length);
+//		for (int i = 0; i < doc.getElementsByClass("name fn").size(); i++) {
+//			shopNames[i] = doc.getElementsByClass("name fn").get(i).text();
+//
+//			if (i < (doc.getElementsByClass("box-flex fd-row ai-center").size())) {
+//				shopPromos[i] = doc.getElementsByClass("box-flex fd-row ai-center").get(i).text();
+//				System.out.println("Space\t:" + i + "/" + shopNames.length + ",  \t" + shopNames[i] + "\t\tPromo\t\t:"
+//						+ i + "/" + shopPromos.length + "\t\tPromo\t\t:" + shopPromos[i]);
+//			} else {
+//				System.out.println("Space\t:" + i + "/" + shopNames.length + ",  \t" + shopNames[i]);
+//			}
+//		}
 
 //		Elements prices = doc.select("box-flex fd-row ai-center");
 //		System.out.println(names.size() + "\tshops in Wanchai");
@@ -170,5 +246,3 @@ public class PandaCrwal {
 //			System.out.println(i + "\tname:\t" + prodIce[i].price + "\tprice:\t" + prodIce[i].name);
 //			driver.quit();
 //		} // ---------------------------------------End of Dairy Product
-
-}
